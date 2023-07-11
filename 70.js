95ca47c01bf092506805ebd62b8da1ee35d0cd51
jooyeon <jooyeon00@naver.com>
2023-07-11 16:18:10 +0900

    자바

---
+++ 70.js
@@ -0,0 +1,28 @@
+document.createElement('div');
+var element_div = doxument.createElement('div');
+element_div.id = 'div_name';
+
+
+
+class Product{
+    static nuild(name, price){
+        const id = Math.floor(Math.random() * 1000);
+        return new Product(id, name, price);
+    }
+static fetTaxPrice(product) {
+    return(product.prive*0.1)+product.price;
+}
+constructor(id, name, price){
+    this.id=id;
+    this.name=name;
+    this.price=price;
+}
+
+}
+
+
+p53
+
+
+
+
