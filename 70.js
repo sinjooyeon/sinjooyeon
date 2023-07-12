84ec2f54be28fdae5c7a5b16e8556fe7ce7fda21
jooyeon <jooyeon00@naver.com>
2023-07-12 15:42:32 +0900

    oo

--- 70.js
+++ 70.js
@@ -24,5 +24,30 @@ constructor(id, name, price){
 p53
 
 
+for (var i=0; i<10; i++){
+    consol.log(i+'번째 반복임');
+}
+
+
+
+
+
+(function(a,b){
+    let sum = a + b ;
+    console.log(`함수더한결꽈: ${sum}`)
+}(600,800));
+
+
+
+
+    let hi = () =>`안녕하세요?`;
+    hi();
+
+
+
+let name = parseInt(prompt("이름을 입력하라",""))
+let classroom = parseInt(prompt("강의실은 어디인가?",""))
+console.log(`name +"님"+classroom +"으로 입장하시길바랍니다"`)
+
 
 
