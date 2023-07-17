b564863a73b4fd7db1fd47b8585bfe2e299547e1
jooyeon <jooyeon00@naver.com>
2023-07-17 15:37:04 +0900

    230717

---
+++ 230717 practice.js
@@ -0,0 +1,251 @@
+
+
+
+let o9= prompt("나이가 몇쩔?","");
+let o8= prompt("다리가 몇개?","");
+
+function add(a,b){
+  return a+b;
+}
+let epu= add(o9,o8);
+console.log (epu);
+
+// practice  practice  practice   practice   practice   practice
+// practice  practice  practice   practice   practice   practice
+
+
+
+let w1= parseInt(prompt("나이가 몇쩔?",""));
+let w2= parseInt(prompt("다리가 몇개?",""));
+
+function add(a,b){
+  return a+b;
+}
+let epw= add(w1,w2);
+console.log (epw);
+
+// practice  practice  practice   practice   practice   practice
+// practice  practice  practice   practice   practice   practice
+
+
+
+
+let g = "hello world"
+g.replace("o","O")
+
+g.charAt(0)
+
+
+function for01(){
+  for(   )
+}
+
+// practice  practice  practice   practice   practice   practice
+// practice  practice  practice   practice   practice   practice
+
+function calcSum(){
+let sum = 0;
+for (let i = 1 ; i<= 10 ; i++){
+  sum +=i;
+}
+console.log (`1부터 10까지 더하면 ${sum}입니다`);
+}
+
+calcSum();
+
+// practice  practice  practice   practice   practice   practice
+// practice  practice  practice   practice   practice   practice
+
+function calcSum() {
+  let sum = 0;
+  for(let i = 1 ; i<= 10 ; i++){
+    sum += i;
+  }
+  console.log(`1부터 10까지 더하면 ${sum}입니다`);
+}
+ calcSum();
+
+// practice  practice  practice   practice   practice   practice
+// practice  practice  practice   practice   practice   practice
+
+
+
+function calcSum(){
+  let sum = 0;
+  for (let i=1; i<=10; i++){
+    sum+=i;
+  }
+  console.log(`1부터 10까지 더해${sum}입니다`)
+}
+calcSum();
+
+// practice  practice  practice   practice   practice   practice
+// practice  practice  practice   practice   practice   practice
+
+
+
+function calcSum(){
+  let sum = 0 ;
+  for (let i=1; i<=10; i++){
+    sum+=i;
+  }
+  console.log(`1부터 10까지 더해 ${sum}이다`)
+}
+calcSum();
+
+// practice  practice  practice   practice   practice   practice
+// practice  practice  practice   practice   practice   practice
+
+
+
+
+
+
+function calcSum(){
+  let sum=0;
+  for (let i = 1; i<=10;i++){
+    sum +=i;
+  }
+console.log(`난 ${sum}이야`)
+
+}
+calcSum();
+
+
+// practice  practice  practice   practice   practice   practice
+// practice  practice  practice   practice   practice   practice
+
+
+
+
+function calcSum(n){
+  let sum=0;
+  for (let i = 1 ; i<=n ; i++){
+    sum +=i;
+  }
+console.log(`난 1부터 ${n}까지 더하믄 ${sum}이징`);
+}
+calcSum(8);
+
+// practice  practice  practice   practice   practice   practice
+// practice  practice  practice   practice   practice   practice
+
+
+
+function calcSum(n){
+  let sum = 0;
+  for ( let i =1 ; i<=n;i++){
+    sum +=i
+  }
+  return(sum)
+}
+let num =parseInt(prompt("몇까지 더해줄까?"))
+console.log(`1부터 ${num}까지 더해쥼 ${calcSum(num)}이다`)
+
+
+
+
+// practice  practice  practice   practice   practice   practice
+// practice  practice  practice   practice   practice   practice
+
+
+let num1 = parseInt(prompt("나이?"));
+let num2 = parseInt(prompt("생일 몇월?"));
+
+function add(a,c){
+  return a*c;
+}
+let tt= add(num1, num2);
+console.log(tt);
+
+
+
+
+function calcSum(n){
+  let sum = 0;
+  for (let i= 1;  i<=n; i++){
+    sum += i;
+  }
+return(sum)
+}
+
+let num = parseInt(prompt("나이는?"));
+console.log(`1부터 ${num}까지 더함 ${calcSum(num)}이다`);
+
+
+ 
+
+//계산기 만들어봄
+
+let num1 = parseInt(prompt("앞의 숫자"));
+let num2 = prompt("연산 +_*/");
+let num3 = parseInt(prompt("뒤의 숫자"));
+
+function add(a,b,c){
+  if (b =='+'){
+   console.log(a+c);
+  }else if(b=='-'){
+    console.log(a-c);
+  }else if(b=='*'){
+    console.log(a*c);
+  }else if(b=='/'){
+    console.log(a/c);
+  }
+  }
+
+let u = add(num1,num2,num3);
+
+
+
+///////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
