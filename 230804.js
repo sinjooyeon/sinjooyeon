512d215f11f6c5e72cf76fe3240e6e8414e38553
jooyeon <jooyeon00@naver.com>
2023-08-16 17:51:34 +0900

    프로토타입

--- 230804.js
+++ 230804.js
@@ -154,3 +154,54 @@ fruit.splice(1, 0, 'banana', 'tomato', 'melon', 'watermelon', 'peach', 'grape');
 fruit.splice(3, 2);
 
 
+const board = [
+  ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'],
+  ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
+  [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+  [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+  [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+  [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+  ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
+  ['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'],
+];
+
+console.log(board.join('\n') + '\n\n');
+
+// 폰을 앞으로 두 칸 전진
+board[4][4] = board[6][4];
+board[6][4] = ' ';
+console.log(board.join('\n'));
+
+
+
+
+
+function Book(title, price) {
+  this.title = title;
+  this.price = price;
+}
+Book.prototype.buy = function() {
+  console.log(`${this.title}을 (를) ${this.price}원에 구입하였습니다`);
+};
+
+const book1 = new Book('ABCDE', 10000);
+book1.buy();
+
+
+
+function Textbook(title, price, major) {
+  Book.call(this, title, price);
+  this.major = major;
+}
+
+Textbook.prototype.buyTextbook = function() {
+  console.log(`${this.major}전공서적, ${this.title}을 구매하였습니다`);
+};
+
+
+Object.setPrototypeOf(Textbook.prototype, book.prototype);
+
+
+const book2 = new Textbook('알고리즘', 5000, '컴퓨터공학');
+book2.buyTextbook();
+book2.buy();
