d92e08552d3d727631b05416c75f99141a7ea720
jooyeon <jooyeon00@naver.com>
2023-08-21 15:57:25 +0900

    객체

--- 230804.js
+++ 230804.js
@@ -251,7 +251,21 @@ for (const [index, element] of candy.entries()) {
 
 
 
+var address = 'Seoul';
+var members = {};
+var addFamily = function(age, name, role) {
+  this.members[role] = {age: age, name: name};
+};
+var getHeadcount = function() {
+  return Object.keys(this.members).length;
+};
 
+var family = {address: address, members: members, addFamily: addFamily, getHeadcount: getHeadcount};
+
+family.addFamily(30, 'chloe', 'aunt');
+family.addFamily(3, 'lyn', 'niece');
+family.addFamily(10, 'dangdangi', 'dog');
+console.log(family.getHeadcount());
 
 
 
