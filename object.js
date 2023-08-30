c0596541b6607a175c674c1e8497e09b3f773f05
jooyeon <jooyeon00@naver.com>
2023-08-30 14:57:21 +0900

    객체 할당

--- object.js
+++ object.js
@@ -79,6 +79,29 @@ const calc = new Calculator(10);
 calc.add(5).subtract(3);
 console.log(calc.value); // 출력: 12
 
+/**
+ * 객체 할당
+ */
+const target = {a: 1, b: 2};
+const source = {b: 4, c: 5};
+
+const returnedTarget = Object.assign(target, source);
+
+console.log(target);
+// Expected output: Object { a: 1, b: 4, c: 5 }
+
+console.log(returnedTarget === target);
+// Expected output: true
+
+
+
+
+
+
+
+
+
+
 
 
 
