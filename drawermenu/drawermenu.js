7fec3562899b73fca2726fa2f4e2820fed9ed1c0
jooyeon <jooyeon00@naver.com>
2023-09-07 16:24:04 +0900

    숨김메뉴만들기

---
+++ drawermenu/drawermenu.js
@@ -0,0 +1,23 @@
+function Drawer(el, open = false) {
+  this.el = el;
+  this.isOpen = open;
+
+  (this.isOpen) ? this.open() : this.close();
+}
+Drawer.prototype.open = function () {
+  this.isOpen = true;
+  this.el.style.transform = 'translate(0px)';
+};
+Drawer.prototype.close = function () {
+  this.isOpen = false;
+  this.el.style.transform = 'translate(220px)';
+};
+
+const sideMenu = new Drawer(document.querySelector('.drawer'));
+document.getElementById('drawer-opener').addEventListener('click', (e) => {
+  if (!sideMenu.isOpen) {
+    sideMenu.open();
+  } else {
+    sideMenu.close();
+  }
+});
