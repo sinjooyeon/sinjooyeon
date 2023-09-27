1ad09adb9df1e9231e0636d7360d5d35e0cf3385
jooyeon <jooyeon00@naver.com>
2023-09-27 10:39:41 +0900

    메뉴바를 위에서 열리게 해보기

--- drawermenu/drawermenu.js
+++ drawermenu/drawermenu.js
@@ -1,16 +1,20 @@
+debugger;
 function Drawer(el, open = false) {
+  debugger;
   this.el = el;
   this.isOpen = open;
 
   (this.isOpen) ? this.open() : this.close();
 }
 Drawer.prototype.open = function() {
+  debugger;
   this.isOpen = true;
-  this.el.style.transform = 'translate(0px)';
+  this.el.style.transform = 'translate(0px,0px)';
 };
 Drawer.prototype.close = function() {
+  debugger;
   this.isOpen = false;
-  this.el.style.transform = 'translate(220px)';
+  this.el.style.transform = 'translate(0px,-100%)';
 };
 
 const sideMenu = new Drawer(document.querySelector('.drawer'));
