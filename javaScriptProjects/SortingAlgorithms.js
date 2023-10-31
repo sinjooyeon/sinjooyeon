d738c4ac0a265379be56ce54321c5a543e7be77e
jooyeon <jooyeon00@naver.com>
2023-10-31 15:40:37 +0900

    Sort알고리즘

---
+++ javaScriptProjects/SortingAlgorithms.js
@@ -0,0 +1,46 @@
+function quickSort(array, start, end) {
+  if (start === undefined) {
+    start = 0;
+    end = array.length - 1;
+  } else if (start >= end) {
+    return array;
+  }
+  var rStart = start, rEnd = end;
+  var pivot = array[Math.floor(Math.random() * (end - start + 1) + start)];
+  while (start < end) {
+    while (array[start] <= pivot) {
+      start++;
+    }
+    while (array[end] > pivot) {
+      end--;
+    }
+    if (start < end) {
+      var temp = array[start];
+      array[start] = array[end];
+      array[end] = temp;
+    }
+  }
+  quickSort(array, rStart, start - 1);
+  quickSort(array, start, rEnd);
+}
+
+
+
+function bubbleSort(arr) {
+  for (var i = 0; i < arr.length; i++) {
+    for (var j = 0; j < (arr.length - i - 1); j++) {
+      if (arr[j] > arr[j + 1]) {
+        var temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+}
+
+
+
+
+
+
+
---
+++ javaScriptProjects/sort.js
@@ -0,0 +1,26 @@
+function selectionSort(arr) {
+  const n = arr.length;
+
+  for (let i = 0; i < n - 1; i++) {
+    let minIndex = i;
+
+    for (let j = i + 1; j < n; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+
+    if (minIndex !== i) {
+      const temp = arr[i];
+      arr[i] = arr[minIndex];
+      arr[minIndex] = temp;
+    }
+  }
+
+  return arr;
+}
+
+// 사용 예제
+const arrayToSort = [64, 34, 25, 12, 22, 11, 90];
+selectionSort(arrayToSort);
+console.log(arrayToSort); // 정렬된 배열 출력
