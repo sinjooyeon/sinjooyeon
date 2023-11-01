29445d4dda374d3f5d8b5c82e9927cdddb5817a8
jooyeon <jooyeon00@naver.com>
2023-11-01 16:41:12 +0900

    정렬 알고리즘 추가

--- javaScriptProjects/SortingAlgorithms.js
+++ javaScriptProjects/SortingAlgorithms.js
@@ -26,6 +26,28 @@ function quickSort(array, start, end) {
 
 
 
+
+function selectionSort(arr) {
+  for (var i = 0; i < arr.length; i++) {
+    let min = i;
+    for (var j = i + 1; j < arr.length; j++) {
+      if (arr[min] > arr[j]) {
+        min = j;
+      }
+    }
+    if (i != min) {
+      [arr[i], arr[min]] = [arr[min], arr[i]];
+    }
+  }
+  return arr;
+}
+
+
+
+
+
+
+
 function bubbleSort(arr) {
   for (var i = 0; i < arr.length; i++) {
     for (var j = 0; j < (arr.length - i - 1); j++) {
@@ -37,10 +59,5 @@ function bubbleSort(arr) {
     }
   }
 }
-
-
-
-
-
 
 
