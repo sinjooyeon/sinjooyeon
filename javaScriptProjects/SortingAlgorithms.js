5e9597dc797e398f39bfec9ab7ee61856d872796
jooyeon <jooyeon00@naver.com>
2023-11-07 16:19:31 +0900

    insertionSort 정렬 알고리즘 추가

--- javaScriptProjects/SortingAlgorithms.js
+++ javaScriptProjects/SortingAlgorithms.js
@@ -27,36 +27,11 @@ function quickSort(array, start, end) {
 
 
 
-function selectionSort(arr) {
-  for (var i = 0; i < arr.length; i++) {
-    let min = i;
-    for (var j = i + 1; j < arr.length; j++) {
-      if (arr[min] > arr[j]) {
-        min = j;
-      }
-    }
-    if (i != min) {
-      [arr[i], arr[min]] = [arr[min], arr[i]];
-    }
-  }
-  return arr;
-}
-
-
-
-
-
-
-
-
-
-
-
 function bubbleSort(arr) {
-  for (var i = 0; i < arr.length; i++) {
-    for (var j = 0; j < (arr.length - i - 1); j++) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < (arr.length - i - 1); j++) {
       if (arr[j] > arr[j + 1]) {
-        var temp = arr[j];
+        let temp = arr[j];
         arr[j] = arr[j + 1];
         arr[j + 1] = temp;
       }
@@ -64,37 +39,95 @@ function bubbleSort(arr) {
   }
 }
 
-
-
-
-
-
-function mergeSort(array) {
-  const half = array.length / 2;
-
-  if (array.length < 2) {
-    return array;
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let min = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[min] > arr[j]) {
+        min = j;
+      }
+    }
+    if (i != min) {
+      [arr[i], arr[min]] = [arr[min], arr[i]]
+    }
   }
-
-  const left = array.splice(0, half);
-  return merge(mergeSort(left), mergeSort(array));
+  return arr
 }
 
-function merge(left, right) {
-  const arr = [];
 
-  while (left.length && right.length) {
-    if (left[0] < right[0]) {
-      arr.push(left.shift());
+
+
+function insertionSort2(arr) {
+  let i, j, temp;
+  for (let i = 0; i < arr.length; i++) {
+    j = i;
+    while (arr[j] > arr[j + 1]) {
+      temp = arr[j];
+      arr[j] = arr[j + 1];
+      arr[j + 1] = temp;
+      j--;
+    }
+
+  }
+}
+
+
+
+function insertionSort(arr) {
+  let i, key, j;
+  for (i = 1; i < arr.length; i++) {
+    key = arr[i];
+    j = i - 1;
+
+    while (j >= 0 && arr[j] > key) {
+      arr[j + 1] = arr[j];
+      j = j - 1;
+    }
+    arr[j + 1] = key;
+  }
+}
+
+function quickSort2(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  const pivot = arr[0];
+  const left = [];
+  const right = [];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < pivot) {
+      left.push(arr[i]);
     } else {
-      arr.push(right.shift());
+      right.push(arr[i]);
     }
   }
 
-  return [...arr, ...left, ...right];
+  return [...quickSort(left), pivot, ...quickSort(right)];
 }
 
+const unsortedArray = [5, 2, 9, 3, 6, 8, 1, 7];
+const sortedArray = quickSort(unsortedArray);
+console.log(sortedArray);
+
+const apikey = '46f80a02ecae410460d59960ded6e1c6';
+
+const weatherDataEl = document.getElementById('weather-data');
+const cityInputEl = document.getElementById('city-input');
+const formEl = document.querySelector('form');
+
+formEl.addEventLIstener('submit', (e) => {
+  e.preventDefault();
+  const cityValue = cityInputEl.ariaValueMax;
+  getWeatherData(cityValue);
+});
 
 
+async function getWeatherData(cityValue) {
+  try {
+    const reponse = await fetch(
 
-
+    )
+  }
+}
