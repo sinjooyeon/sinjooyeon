2860e900a2d5563ac727bae7bbf8b188bb87f1fa
jooyeon <jooyeon00@naver.com>
2023-12-06 09:49:41 +0900

    쇼핑몰수정

--- vegetable-shop/vegetablecartpage.js
+++ vegetable-shop/vegetablecartpage.js
@@ -1,13 +1,12 @@
 
-import {selectedVegetables} from './cart.js'; 
-import { vegetable } from './vegetable.js';
+import {vegetable} from './vegetable.js';
 
 const selectedVegetablesString = localStorage.getItem('selectedVegetables');
 const selected = JSON.parse(selectedVegetablesString);
 console.log(selected);
 const cartListElement = document.getElementById('basket');
 
-// 중복된 상품 수량 더하기를 위한 객체
+
 const cartItems = {};
 
 selected.forEach(cartProduct => {
@@ -15,12 +14,10 @@ selected.forEach(cartProduct => {
   const productInfo = vegetable.find(product => product.id === productId);
 
   if (productInfo) {
-    // 이미 선택된 상품인 경우 수량만 증가
     if (cartItems[productId]) {
       cartItems[productId].quantity += cartProduct.quantity;
     } else {
-      // 처음 선택된 상품인 경우 새로운 객체 생성
-      cartItems[productId] = { ...cartProduct, ...productInfo };
+      cartItems[productId] = {...cartProduct, ...productInfo};
     }
   } else {
     console.error(`Product with ID ${productId} not found in the products array.`);
@@ -54,64 +51,11 @@ cartItemsArray.forEach(cartProduct => {
 // 최종적으로 중복 처리된 상품 리스트를 화면에 추가
 console.log(cartItemsArray);
 
+function dropdownChanged() {
+  let selectedValue = document.getElementById("dropdown").value;
+  document.getElementById("result").innerHTML = "선택된 항목: " + selectedValue;
+}
 
+document.getElementById("dropdown").addEventListener("change", dropdownChanged);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import {vegetable} from './vegetable.js';
-
-// const selectedVegetablesString = localStorage.getItem('selectedVegetables');
-// const selected = JSON.parse(selectedVegetablesString);
-// console.log(selected);
-// const cartListElement = document.getElementById('basket');
-
-// selected.forEach(cartProduct => {
-  
-//   const productInfo = vegetable.find(product => product.id === cartProduct.id);
-
-//   if (productInfo) {
-//     const listItem = document.createElement('div');
-//     listItem.innerHTML = `
-//       <div class="orderCheck">
-//         <div>
-//           <div class="content">
-//             <div class="small-pic">
-//               <img src="${productInfo.image}" alt="">
-//             </div>
-//             <div class="text">
-//               <p>상품: ${productInfo.id}</p>
-//               <p>제품설명 : ${productInfo.description}</p>
-//               <p>수량: ${cartProduct.quantity}개</p>
-//               <p>가격: ${productInfo.price}원</p>
-//             </div>
-//           </div>
-//         </div>
-//       </div>`;
-//     cartListElement.appendChild(listItem);
-//   } else {
-    
-//     console.error(`Product with ID ${cartProduct.productId} not found in the products array.`);
-//   }
-// });
 
